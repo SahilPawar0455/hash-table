4ef7734a89f64112145c32a7643689f2f2219c6b
SAHIL0455 <sahilpawar1004@gmail.com>
2022-09-02 12:32:50 +0530

    Initial Meassages

--- src/com/bridgelabz/HashTable.java
+++ src/com/bridgelabz/HashTable.java
@@ -2,6 +2,6 @@ package com.bridgelabz;
 
 public class HashTable {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("Welcome to Hash Table");
     }
 }
